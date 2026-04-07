89a898f26b2d9ed00bb1b7f52bd68eb13110141b
Anshu Shee <anshu.shee.cg@gmail.com>
2026-04-07 17:02:16 +0530

    Time: 3 ms (60.18%), Space: 11.3 MB (88.87%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void backtrack(vector<int>& nums, vector<bool>& visited, 
+                   vector<int>& temp, vector<vector<int>>& result) {
+        
+        if (temp.size() == nums.size()) {
+            result.push_back(temp);
+            return;
+        }
+        
+        for (int i = 0; i < nums.size(); i++) {
+            
+          
+            if (visited[i]) continue;
+            
+            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])
+                continue;
+            
+
+            visited[i] = true;
+            temp.push_back(nums[i]);
+            
+
+            backtrack(nums, visited, temp, result);
+            
+            // Backtrack
+            temp.pop_back();
+            visited[i] = false;
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> temp;
+        vector<bool> visited(nums.size(), false);
+        
+        sort(nums.begin(), nums.end());
+        
+        backtrack(nums, visited, temp, result);
+        return result;
+    }
+};
