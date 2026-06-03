f951b5a05e96e79933666d7b9aba4baa98792caa
Anshu Shee <anshu.shee.cg@gmail.com>
2026-06-03 11:00:39 +0530

    Time: 0 ms (100%), Space: 9.1 MB (95.57%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+
+        int freq[26] = {0};
+
+        for(char ch : t)
+        {
+            freq[ch - 'a']++;
+        }
+
+        for(char ch : s)
+        {
+            freq[ch - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++)
+        {
+            if(freq[i] > 0)
+            {
+                return i + 'a';
+            }
+        }
+
+        return ' ';
+    }
+};
