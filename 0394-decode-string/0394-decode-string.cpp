dfbd58faf5006913bc13400c7f0055d0dd9d6a32
Anshu Shee <anshu.shee.cg@gmail.com>
2026-06-04 11:05:38 +0530

    Time: 0 ms (100%), Space: 9.2 MB (79.77%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    string solve(string &s, int &i)
+    {
+        string result = "";
+
+        while(i < s.length() && s[i] != ']')
+        {
+            if(isalpha(s[i]))
+            {
+                result += s[i];
+                i++;
+            }
+            else if(isdigit(s[i]))
+            {
+                int num = 0;
+
+                while(i < s.length() && isdigit(s[i]))
+                {
+                    num = num * 10 + (s[i] - '0');
+                    i++;
+                }
+
+                i++; // skip '['
+
+                string decoded = solve(s, i);
+
+                i++; // skip ']'
+
+                while(num--)
+                {
+                    result += decoded;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    string decodeString(string s)
+    {
+        int i = 0;
+        return solve(s, i);
+    }
+};
