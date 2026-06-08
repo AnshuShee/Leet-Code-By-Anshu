50eb890348491dc6d2ec621f5ef55a39eed69ac5
Anshu Shee <anshu.shee.cg@gmail.com>
2026-06-08 22:55:52 +0530

    Time: 0 ms (100%), Space: 11.4 MB (86.28%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        
-        for (int i = n - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(digits[i]< 9){
                 digits[i]++;
                 return digits;
             }
-            digits[i] = 0; // carry over
+            digits[i] = 0;
         }
-        
-        // if all digits were 9
         digits.insert(digits.begin(), 1);
         return digits;
     }
