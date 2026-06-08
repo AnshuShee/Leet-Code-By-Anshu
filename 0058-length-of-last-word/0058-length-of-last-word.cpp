1ae69ca68953d56e60bc22b18bce065452496e8a
Anshu Shee <anshu.shee.cg@gmail.com>
2026-06-08 22:26:14 +0530

    Time: 0 ms (100%), Space: 9 MB (32.42%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i = s.length() - 1;
-        int count = 0;
+        int length = 0;
+        int i = s.length() -1 ;
 
-    
-        while (i >= 0 && s[i] == ' ') {
+        while (i >= 0 && s[i] == ' '){
             i--;
         }
-
-      
-        while (i >= 0 && s[i] != ' ') {
-            count++;
+        while ( i >= 0 && s[i] != ' ' ){
+            length ++;
             i--;
         }
-
-        return count;
+       return length; 
     }
 };
