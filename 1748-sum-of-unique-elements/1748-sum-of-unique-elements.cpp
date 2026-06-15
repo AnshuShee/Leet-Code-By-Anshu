268d28d4d7328d89c408c4cb495aaae4f2121ddd
Anshu Shee <anshu.shee.cg@gmail.com>
2026-06-15 11:38:59 +0530

    Time: 0 ms (100%), Space: 10.7 MB (44.31%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
-        unordered_set<int> unique, duplicate;
+        map<int, int> mp;
 
         for (int num : nums) {
-            if (duplicate.count(num))
-                continue;
-
-            if (unique.count(num)) {
-                unique.erase(num);
-                duplicate.insert(num);
-            } else {
-                unique.insert(num);
-            }
+            mp[num]++;
         }
 
         int sum = 0;
-        for (int num : unique) {
-            sum += num;
+
+        for (auto it : mp) {
+            if (it.second == 1) {
+                sum += it.first;
+            }
         }
 
         return sum;
