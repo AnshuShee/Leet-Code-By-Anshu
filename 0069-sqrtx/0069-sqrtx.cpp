3856b5a430a9f79b59ede7a98be5b5beed4ea674
Anshu Shee <anshu.shee.cg@gmail.com>
2026-06-18 15:53:46 +0530

    Time: 15 ms (10.2%), Space: 8.5 MB (48.77%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0 || x == 1) return x;
+        int ans = 0;
 
-        int left = 1, right = x, ans = 0;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            // Avoid overflow using division instead of mid*mid
-            if (mid <= x / mid) {
-                ans = mid;        // possible answer
-                left = mid + 1;   // try bigger
-            } else {
-                right = mid - 1;  // go smaller
-            }
+        for(long long i = 0; i * i <= x; i++) {
+            ans = i;
         }
 
         return ans;
