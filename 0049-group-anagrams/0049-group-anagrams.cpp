b26cb40ba20d2bb2ce4a27cdcabd7fa1c29d4080
Anshu Shee <anshu.shee.cg@gmail.com>
2026-06-18 11:01:50 +0530

    Time: 24 ms (26.56%), Space: 26.1 MB (37.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,21 +3,22 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
         unordered_map<string, vector<string>> mp;
-
-        for (int i = 0; i < strs.size(); i++) {
+        for(int i=0; i<strs.size(); i++){
             string word = strs[i];
-            
             string sortedWord = word;
-            sort(sortedWord.begin(), sortedWord.end()); 
-            mp[sortedWord].push_back(word); 
+            sort(sortedWord.begin(), sortedWord.end());
+            mp[sortedWord].push_back(word);
         }
 
         vector<vector<string>> result;
 
-        for (auto it : mp) {
+        for(auto it : mp){
             result.push_back(it.second);
-        }
 
+        }
         return result;
+
+
+        
     }
 };
