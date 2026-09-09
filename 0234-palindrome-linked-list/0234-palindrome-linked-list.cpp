fb371a5ae2f85a01068c445e81438915f3ce14b1
Anshu Shee <anshu.shee.cg@gmail.com>
2026-09-09 14:27:07 +0530

    Time: 0 ms (100%), Space: 118 MB (64.06%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -2,23 +2,67 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
 
-        ListNode* i = head;
-        ListNode* address = NULL;
+        // ListNode* i = head;
+        // ListNode* address = NULL;
 
-        while (i != NULL) {
+        // while (i != NULL) {
 
-            ListNode* newNode = new ListNode();
-            newNode->val = i->val;
-            newNode->next = address;
+        //     ListNode* newNode = new ListNode();
+        //     newNode->val = i->val;
+        //     newNode->next = address;
 
-            address = newNode;
-            i = i->next;
+        //     address = newNode;
+        //     i = i->next;
+        // }
+
+        // i = head;
+        // ListNode* j = address;
+
+        // while (i != NULL) {
+
+        //     if (i->val != j->val) {
+        //         return false;
+        //     }
+
+        //     i = i->next;
+        //     j = j->next;
+        // }
+
+        // return true;
+
+           if (head == NULL || head->next == NULL) {
+            return true;
         }
 
-        i = head;
-        ListNode* j = address;
+        ListNode* i = head;
+        ListNode* j = head;
+
+        // Find middle
+        while (j->next != NULL && j->next->next != NULL) {
+            i = i->next;
+            j = j->next->next;
+        }
+
+        // i is middle
+        ListNode* address = NULL;
+
+        // Reverse second half
+        i = i->next;
 
         while (i != NULL) {
+            ListNode* temp = i->next;
+
+            i->next = address;
+            address = i;
+
+            i = temp;
+        }
+
+        // Compare first half and reversed second half
+        i = head;
+        j = address;
+
+        while (j != NULL) {
 
             if (i->val != j->val) {
                 return false;
@@ -29,5 +73,6 @@ public:
         }
 
         return true;
+
     }
 };
